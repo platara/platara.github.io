46d94f72b24be820a0c81e1fd3c433d9799c7ff6
platara <plat0033@algonquinlive.com>
2023-01-18 21:37:05 -0500

    add main js

---
+++ main.js
@@ -0,0 +1,58 @@
+//1.Variables
+// Valid name variables
+// variable name may only start with a letter, the dollar sign ($), or an underscore (_).
+// variable name may contain letters, numbers, dollar signs, and underscores, but no other characters, like periods or hyphens.
+// can't start a variable name with a number
+// variable names are case-sensitive
+
+
+//Avoid Keywords
+// reserved words that are part of the language
+
+
+//Avoid variable names already assigned
+// browsers define global standard variables: alert, document, window
+
+
+//2. let
+// variables that can be reasign. used when it is known that a variable value will change.
+
+
+//3. const
+// variables that cannot be reasign. These are written completely in uppercase
+
+//4. Strings
+// sequences of characters used to represent text
+// string concatenation
+// template literals
+
+//5. Number
+// represent floating-point numbers.
+
+// definition of an integer
+// definition of a decimal
+// definition of a negative integer
+// definition of a negative decimal
+// separator for decimal representation
+// separator for decimal representation
+
+// JavaScript includes useful methods and functions for working with numbers.
+
+//6. Boolean
+// data type that can only have two values: true or false.
+
+//7. Arrays
+// can contain several values. It's an ordered list of values.
+// array literal notation []
+// index-based structure (every element in an array is stored at a particular index/position)
+// Multidimensional Arrays
+
+//8. Objects
+// can contain several values. Can be seen as an unordered list of values wih a key and a value pair.
+// In the context of objects, variables are called properties or attributes, and functions are referred to as methods.
+// object literal notation {}
+
+
+//9. Operators and expresions
+
+
